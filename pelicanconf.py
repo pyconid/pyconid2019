0c559bc70126a50151bbbf5d08abf1eb98a914c5
Dwi Sulfahnur <hello@dwisulfahnur.com>
2019-08-17 11:31:31 +0700

    Add cfp page to the Menu Items (Navbar)

--- pelicanconf.py
+++ pelicanconf.py
@@ -53,6 +53,7 @@ ARTICLE_SAVE_AS = 'articles/{slug}.html'
 # Menu Items configurations
 MENUITEMS = (
     ('About', 'about'),
+    ('Speak at Pycon ID', 'call-for-paper'),
     ('Speakers', 'speakers'),
     ('Schedule', 'schedule'),
     ('Blog', 'blog'),
