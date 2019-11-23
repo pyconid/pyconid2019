c98b3011574e744add487ca6c23b01cc968f008e
alifakbar111 <alifakbar111@gmail.com>
2019-11-24 01:04:26 +0700

    deactivated timer

--- themes/eventalk/static/js/main.js
+++ themes/eventalk/static/js/main.js
@@ -322,33 +322,33 @@
 	});
 
 
-function makeTimer() {
+// function makeTimer() {
 
-		var endTime = new Date("23 November 2019 10:00:00 GMT+07:00");			
-		endTime = (Date.parse(endTime) / 1000);
+// 		var endTime = new Date("23 November 2019 10:00:00 GMT+07:00");			
+// 		endTime = (Date.parse(endTime) / 1000);
 
-		var now = new Date();
-		now = (Date.parse(now) / 1000);
+// 		var now = new Date();
+// 		now = (Date.parse(now) / 1000);
 
-		var timeLeft = endTime - now;
+// 		var timeLeft = endTime - now;
 
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+// 		var days = Math.floor(timeLeft / 86400); 
+// 		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+// 		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+// 		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
+// 		if (hours < "10") { hours = "0" + hours; }
+// 		if (minutes < "10") { minutes = "0" + minutes; }
+// 		if (seconds < "10") { seconds = "0" + seconds; }
 
-		$("#days").html(days + "<span>Days</span>");
-		$("#hours").html(hours + "<span>Hours</span>");
-		$("#minutes").html(minutes + "<span>Minutes</span>");
-		$("#seconds").html(seconds + "<span>Seconds</span>");		
+// 		$("#days").html(days + "<span>Days</span>");
+// 		$("#hours").html(hours + "<span>Hours</span>");
+// 		$("#minutes").html(minutes + "<span>Minutes</span>");
+// 		$("#seconds").html(seconds + "<span>Seconds</span>");		
 
-}
+// }
 
-setInterval(function() { makeTimer(); }, 1000);
+// setInterval(function() { makeTimer(); }, 1000);
 
 
 
